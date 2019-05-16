b017f29a94984a802cc4051156564e5a69ebeca4
srqrme <srqrme@gmail.com>
2019-05-16 18:04:38 -0400

    update file with user validations and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, presence: true,
+                         length: { minimum: 6 } 
+
+    has_secure_password
+                
+
 end
