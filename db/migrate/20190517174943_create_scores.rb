ec19ae094199fa3f65fb13f6d5aa5c8120347e0b
srqrme <srqrme@gmail.com>
2019-05-17 13:50:06 -0400

    generate new Score resource, model, and controller

---
+++ db/migrate/20190517174943_create_scores.rb
@@ -0,0 +1,12 @@
+class CreateScores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :scores do |t|
+      t.integer :user_id
+      t.integer :whiskey_id
+      t.text :comments
+      t.integer :review
+
+      t.timestamps
+    end
+  end
+end
